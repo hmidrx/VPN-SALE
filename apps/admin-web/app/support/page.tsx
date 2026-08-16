acb8393105637553a9388b9541f3f35a86dadf0c
hmidrx <hmid3x@gmail.com>
2026-08-16 20:19:19 +0330

    BOT-3E: surface SLA inbox in support admin

--- apps/admin-web/app/support/page.tsx
+++ apps/admin-web/app/support/page.tsx
@@ -1,2 +1,11 @@
 import { SupportConsole } from "../../src/support/SupportConsole";
-export default function Page() { return <SupportConsole />; }
+import { SupportSlaInbox } from "../../src/support/SupportSlaInbox";
+
+export default function Page() {
+  return (
+    <>
+      <SupportSlaInbox />
+      <SupportConsole />
+    </>
+  );
+}
